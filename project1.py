e4c18ad4f4a142aba214cbbc875c14ccb7ef793b
rachellee0114 <71680200+rachellee0114@users.noreply.github.com>
2020-09-30 16:32:49 -0700

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,25 @@
+def displayIntro():
+  #put an Introduction message to the users
+  pass 
+
+def displayNonProfits():
+  #print all the non-profits to the screen numerically. For Example:
+#    1. World Central Kitchen
+#    2. Crisis Text Line
+#    3. Heart to Heart International
+  pass 
+
+
+def main():
+	pass #remove me and everywhere else I appear
+
+	#steps: 
+	#1. welcome the user using a unique Intro Message (use a function for this)
+	#2. create a variable for each nonprofit/charity that keeps track of how much total money has been donated
+	#3. display all the non-profits to the user and ask which one they would like to donate to 
+	#4. then ask the user how much money they would like to donate to that specific charity chosen in step 3
+	#5. update the variable created in step 2 with whatever amount the user wanted to donated
+			#for example: missionbit = 0 -> user wants to donate 100 so you would add 100 to missionbit. missionbit is now 100
+	#6. now display all the nonprofits with their new total amounts donated
+    #7. repeat this process until the user wants to stop donating (only do this if you cover while loops)
+	#push to github after every single step
